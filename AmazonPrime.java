ba5d307a34bb45a5b9e912b4bfd213791d2c8ca7
YAMA <srinathxworkz@gmail.com>
2022-06-28 11:45:53 +0530

    adding variable file

---
+++ AmazonPrime.java
@@ -0,0 +1,55 @@
+class AmazonPrime{
+static String movie1="Roberrt";
+static String movie2="Love mocktail";
+static String movie3="99";
+static String movie4="Yuvaratna";
+static String movie5="Radhe Shyam";
+static String movie6="Four Walls";
+static String movie7="K.G.F";
+static String movie8="Ratnan Prapancha";
+static String movie9="Badava Rascal";
+static String movie10="Dia";
+static String movie11="Jai Bhim";
+static String movie12="Law";
+static String movie13="Ninna Sanihake";
+static String movie14="Majili";
+static String movie15="French Biryani";
+static String movie16="Saaho";
+static String movie17="Yajamana";
+static String movie18="Rathavara";
+static String movie19="Odeya";
+static String movie20="Malgudi Days";
+static String movie21="Man Of The Match";
+static String movie22="Old Monk";
+static String movie23="Pushpa";
+static String movie24="Madagaja";
+static String movie25="Rustum";
+public static void main(String[] args){
+System.out.println(movie1);
+System.out.println(movie2);
+System.out.println(movie3);
+System.out.println(movie4);
+System.out.println(movie5);
+System.out.println(movie6);
+System.out.println(movie7);
+System.out.println(movie8);
+System.out.println(movie9);
+System.out.println(movie10);
+System.out.println(movie11);
+System.out.println(movie12);
+System.out.println(movie13);
+System.out.println(movie14);
+System.out.println(movie15);
+System.out.println(movie16);
+System.out.println(movie17);
+System.out.println(movie18);
+System.out.println(movie19);
+System.out.println(movie20);
+System.out.println(movie21);
+System.out.println(movie22);
+System.out.println(movie23);
+System.out.println(movie24);
+System.out.println(movie25);
+}
+}
+
